2a096f7746e330a1d03a289ac35d578ee7bf4993
John Blaine <jblaine@Johns-MacBook-Pro.local>
2018-11-19 17:03:24 -0500

    done

---
+++ U/Menu/zask.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+func2 () {
+    for i in "$1"/*;
+        do
+
+        if [ -f "$i" ]; then
+            printf "File: %s\n\n" "${i}"
+            sips -Z 400 "${i}";
+        else
+            func2 "${i}"
+        fi
+    done
+    return 0
+}
+
+#####################################
+
+for i in *;
+    do
+
+    if [ -d "$i" ]; then
+
+        printf "Modify %s?  -> " "${i}"
+        read ANSWER
+
+
+        if [ "$ANSWER" == "y" ]; then
+            printf "change it\n\n"
+            func2 "${i}"
+        else
+            printf "no change\n\n"
+        fi
+    fi
+
+done
+
+
+
+
