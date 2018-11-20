3c0ab1f70a2546683b6144d8a2c6832ba068286c
John Blaine <jblaine@Johns-MacBook-Pro.local>
2018-11-20 16:48:11 -0500

    done

--- Bash/checkmissing.sh
+++ Bash/checkmissing.sh
@@ -2,23 +2,24 @@
 
 printf "What is your name?  -> "
 read NAME
+num=0
 
 
 func2 () {
+
     for i in "$1"/*;
         do
-
         if [ -f "$i" ]; then
-            printf "File: %s\n\n" "${i}"
+#            printf "-\n"
 #            sips -Z 600 "${i}";
+            num=$(( $num + 1 ))
         else
 
             if [ -d "$i" ]; then
                 func2 "${i}"
             else
-                printf "Unknown: %s\n\n" "${i}"
+                printf "Unknown: %s\n" "${i}"
             fi
-
         fi
     done
     return 0
@@ -35,6 +36,8 @@ for i in *;
     fi
 
 done
+
+printf "Count: %d\n\n" "${num}"
 
 
 
