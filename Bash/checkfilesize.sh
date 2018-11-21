d366a221fc28f5b4233ff458451aebb5941484b0
John Blaine <jblaine@Johns-MacBook-Pro.local>
2018-11-20 22:06:43 -0500

    done

--- Bash/checkfilesize.sh
+++ Bash/checkfilesize.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+printf "What is your name?  -> "
+read NAME
+num=0
 
 
 func2 () {
+
     for i in "$1"/*;
         do
-
         if [ -f "$i" ]; then
-            printf "File: %s\n\n" "${i}"
             FILESIZE=$(stat -f%z "$i")
+#            printf "Size: %s\n" "${FILESIZE}"
 
             if [ $FILESIZE -lt 1109364 ]; then
                 printf "no Size: %s\n" "${FILESIZE}"
             else
                 printf "do Size: %s\n" "${FILESIZE}"
-                sips -Z 800 "${i}";
             fi
 
-
+            num=$(( $num + 1 ))
         else
+
             if [ -d "$i" ]; then
                 func2 "${i}"
             else
-                printf "Unknown: %s\n\n" "${i}"
+                printf "Unknown: %s\n" "${i}"
             fi
         fi
     done
@@ -35,20 +38,13 @@ for i in *;
     do
 
     if [ -d "$i" ]; then
-
-        printf "Modify %s?  -> " "${i}"
-        read ANSWER
-
-
-        if [ "$ANSWER" == "y" ]; then
-            printf "change it\n\n"
-            func2 "${i}"
-        else
-            printf "no change\n\n"
-        fi
+        #printf "File: %s\n\n" "${i}"
+        func2 "${i}"
     fi
 
 done
+
+printf "Count: %d\n\n" "${num}"
 
 
 
---
+++ iOS/checkfilesize.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+printf "What is your name?  -> "
+read NAME
+num=0
+
+
+func2 () {
+
+    for i in "$1"/*;
+        do
+        if [ -f "$i" ]; then
+            FILESIZE=$(stat -f%z "$i")
+#            printf "Size: %s\n" "${FILESIZE}"
+
+            if [ $FILESIZE -lt 1109364 ]; then
+                printf "no Size: %s %s\n" "${FILESIZE}" "${i}"
+            else
+                printf "do Size: %s %s\n" "${FILESIZE}" "${i}"
+            fi
+
+            num=$(( $num + 1 ))
+        else
+
+            if [ -d "$i" ]; then
+                func2 "${i}"
+            else
+                printf "Unknown: %s\n" "${i}"
+            fi
+        fi
+    done
+    return 0
+}
+
+#####################################
+
+for i in *;
+    do
+
+    if [ -d "$i" ]; then
+        #printf "File: %s\n\n" "${i}"
+        func2 "${i}"
+    fi
+
+done
+
+printf "Count: %d\n\n" "${num}"
+
+
+
+
